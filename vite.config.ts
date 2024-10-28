3417195e25e53354ececc457a32969673ee4c2fd
Puwizz <puwiztubtimngam.newv@gmail.com>
2024-10-28 21:44:58 +0700

    fix bug 4

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  // ตั้ง base ให้เป็นชื่อ repository ของคุณ
-  base: '/My-girlfriend-bd/', // แทนที่ "My-girlfriend-bd" ด้วยชื่อ repository ของคุณ
+
 });
